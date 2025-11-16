11e731cb5b46ec2fd52f10d3d15cf522de0a9134
L00196725 <L00196725@atu.ie>
2025-11-16 22:04:19 +0000

    Monitoring phase: aws CloudWatch

--- task3-pipeline/infraTerraform/main.tf
+++ task3-pipeline/infraTerraform/main.tf
@@ -18,13 +18,15 @@ locals {
 }
 
 resource "aws_instance" "calculate" {       # EC2 instance for calculator + Nginx
-  ami           = "ami-011899242bb902164" # EC2 Ubuntu 20.04 LTS // us-east-1
+  ami           = "ami-011899242bb902164"   # EC2 Ubuntu 20.04 LTS // us-east-1
   instance_type = "t3.micro"
   subnet_id     = module.vpc.public_subnets[0]
   key_name = "task3pipeline-key"
 
   vpc_security_group_ids      = [aws_security_group.calc_sg.id]
   associate_public_ip_address = true
+
+  monitoring = true                         #Enable detailed monitoring in CloudWatch
 
   user_data = <<-EOF
 #!/bin/bash
@@ -109,4 +111,33 @@ resource "aws_security_group" "calc_sg" {
     extraTag = local.extra_tag
     Name     = "calc-sg"
   }
+}
+# CloudWatch alarm to monitor high CPU usage on the calculator EC2 instance
+resource "aws_cloudwatch_metric_alarm" "cpu_high_calculate" {
+  alarm_name          = "calculator-ec2-high-cpu"
+  alarm_description   = "Alarm when EC2 CPU usage is too high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 300                # 300 seconds = 5 minutes
+  statistic           = "Average"
+  threshold           = 70                 # Trigger alarm if CPU > 70%
+  treat_missing_data  = "notBreaching"
+
+  dimensions = {
+    InstanceId = aws_instance.calculate.id
+  }
+
+  
+}
+
+output "calculator_ec2_public_ip" {
+  description = "Public IP address of the calculator EC2 instance"
+  value       = aws_instance.calculate.public_ip
+}
+
+output "calculator_cpu_alarm_name" {
+  description = "Name of the CloudWatch CPU alarm for the calculator EC2 instance"
+  value       = aws_cloudwatch_metric_alarm.cpu_high_calculate.alarm_name
 }
