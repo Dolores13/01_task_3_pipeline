e15af05dff7f7d9d86bcae9146e26e5c3a642edb
L00196725 <L00196725@atu.ie>
2025-11-17 16:59:39 +0000

    video testing

--- task3-pipeline/infraTerraform/main.tf
+++ task3-pipeline/infraTerraform/main.tf
@@ -132,12 +132,4 @@ resource "aws_cloudwatch_metric_alarm" "cpu_high_calculate" {
   
 }
 
-output "calculator_ec2_public_ip" {
-  description = "Public IP address of the calculator EC2 instance"
-  value       = aws_instance.calculate.public_ip
-}
 
-output "calculator_cpu_alarm_name" {
-  description = "Name of the CloudWatch CPU alarm for the calculator EC2 instance"
-  value       = aws_cloudwatch_metric_alarm.cpu_high_calculate.alarm_name
-}
--- task3-pipeline/infraTerraform/networking.tf
+++ task3-pipeline/infraTerraform/networking.tf
@@ -4,8 +4,8 @@ module "vpc"{
     name = "terraform-vpc"
     cidr = "10.0.0.0/16"
     
-    azs = ["us-east-1a", "us-east-1b"]
-    public_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+    azs = ["us-east-1a"]
+    public_subnets = ["10.0.101.0/24"]
 
     enable_vpn_gateway = true
     enable_dns_hostnames = true
